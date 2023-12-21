2ad965ef383d97ff3ee78bd40e2d5a7e4c0286a0
Oliver G. Mueller <oliver@teqneers.de>
2023-12-21 23:31:37 +0000

    debug test failure

--- tests/CliTest.php
+++ tests/CliTest.php
@@ -95,8 +95,10 @@ class CliTest extends TestCase
 
     public function testFileInput(): void
     {
-        $cmd = self::$cmd.' '.escapeshellarg('shamir:share').' '.escapeshellarg('-f').' '.escapeshellarg('tests/secret.txt');
-        $ret = $this->execute($cmd);
+        exec(self::$cmd.' shamir:share -f tests/secret.txt', $output, $ret);
+        var_dump($output);
+        var_dump($ret);
+        $ret = $this->execute(self::$cmd.' shamir:share -f tests/secret.txt');
         self::assertEquals(0, $ret['ret'], 'Non zero return code: '.var_export($ret, true));
         self::assertMatchesRegularExpression('(10201.*)', $ret['std']);
         self::assertMatchesRegularExpression('(10202.*)', $ret['std']);
